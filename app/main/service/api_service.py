514e95ce37ce80e618444ad4acbf33742a82486c
HabibaMoulfi <kh_moulfi@esi.dz>
2024-03-06 02:30:14 +0100

    adding api dto and implementing the create api endpoint

--- app/main/service/api_service.py
+++ app/main/service/api_service.py
@@ -4,6 +4,7 @@ from http import HTTPStatus
 
 from app.main.model.user_model import User 
 from app.main.model.api_category_model import ApiCategory
+from app.main.model.api_model import ApiModel
 from app.main import db, flask_bcrypt
 from app.main.utils.validators import isEmailValid
 from app.main.utils.roles import Role
@@ -106,3 +107,85 @@ class ApiManagement:
                 'error': str(e)
             }
             return response_object, HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+    @staticmethod
+    def create_api(request,data: Dict[str, str]) -> Tuple[Dict[str, Any], int]:
+        try:
+            auth_token = request.headers.get('Authorization')
+            if auth_token:
+                resp = User.decode_auth_token(auth_token)
+                
+                if isinstance(resp, str):
+                    response_object = {
+                    'status': 'fail',
+                    'message': resp
+                    }
+                    return response_object, HTTPStatus.UNAUTHORIZED
+                
+                user = User.query.filter_by(id=resp).first()
+
+                if not user:
+                    response_object = {
+                        'status': 'fail',
+                        'message': 'User does not exist'
+                    }
+                    return response_object, HTTPStatus.NOT_FOUND
+                
+                if not user.check_status('active'):
+                    response_object = {
+                        'status': 'fail',
+                        'message': 'User is not active.'
+                    }
+                    return response_object, HTTPStatus.FORBIDDEN
+
+                
+                category_id = data.get('category_id')
+                category = ApiCategory.query.filter_by(id=category_id).first()
+                if not category:
+                    response_object = {
+                        'status': 'fail',
+                        'message': 'Category does not exist'
+                    }
+                    return response_object, HTTPStatus.NOT_FOUND
+
+
+                    
+                new_api = ApiModel(
+                    name=data.get('name'),
+                    description=data.get('description'),
+                    category_id=category_id,
+                    supplier_id=resp
+                )
+
+                db.session.add(new_api)
+                db.session.commit()
+
+                response_object = {
+                    'data': {
+                        'id': new_api.id,
+                        'name': new_api.name,
+                        'description': new_api.description,
+                        'category_id': new_api.category_id,
+                        'created_at': new_api.created_at.isoformat(),
+                        'updated_at': new_api.updated_at.isoformat()
+                    },
+                    'status': 'success',
+                    'message': 'Successfully created an api.'
+                }
+                return response_object, HTTPStatus.CREATED
+            else:
+                response_object = {
+                    'status': 'fail',
+                    'message': 'Provide a valid auth token.'
+                }
+                return response_object, HTTPStatus.UNAUTHORIZED
+        
+        except Exception as e:
+            response_object = {
+                'status': 'fail',
+                'message': 'Try again',
+                'error': str(e)
+            }
+            return response_object, HTTPStatus.INTERNAL_SERVER_ERROR
+    
