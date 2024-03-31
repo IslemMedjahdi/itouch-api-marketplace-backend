6aec8bc8888948f7a1b8352bb58d586e85a73ba6
IslemMedjahdi <ki_medjahdi@esi.dz>
2024-03-31 03:25:54 +0100

    fix lint

--- app/test/test_category.py
+++ app/test/test_category.py
@@ -3,7 +3,7 @@ from .fixtures.category.add_category import add_category
 from app.main.core.services.api_category_service import ApiCategoryService
 
 
-ApiCategoryService = ApiCategoryService()
+api_category_service = ApiCategoryService()
 
 
 def test_get_all_categories(test_db):
@@ -13,14 +13,14 @@ def test_get_all_categories(test_db):
         description="New Category description",
         user_id=1,
     )
-    categories = ApiCategoryService.get_all_categories()
+    categories = api_category_service.get_all_categories()
     assert len(categories) == 1
     assert categories[0].id == new_category.id
 
 
 def test_create_new_category(test_db):
     data = {"name": "New Category", "description": "New description"}
-    new_category = ApiCategoryService.create_category(data, 1)
+    new_category = api_category_service.create_category(data, 1)
     assert new_category.name == data["name"]
     assert new_category.description == data["description"]
     category = ApiCategory.query.get(new_category.id)
